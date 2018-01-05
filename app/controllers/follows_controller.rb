71c91a702ad949e0b2238baf9d670d4e942a57da
vitorgmw <vitor_martines@hotmail.com>
2018-01-05 11:28:46 -0200

    final aula 2

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,8 +1,34 @@
 class FollowsController < ApplicationController
+  # load_and_authorize_resource
+  before_action :set_user
   
   def create
+    respond_to do |format|
+      if current_user.follow(@user)
+        format.json { render @user, status: :created }
+        format.html { redirect_back fallback_location: root_path, notice: 'User followed with success.' }
+      else
+        format.json { render json: nil, status: :unprocessable_entity }
+        format.html { redirect_back fallback_location: root_path }
+      end
+    end
   end
 
   def destroy
+    respond_to do |format|
+      if current_user.stop_following(@user)
+        format.json { head :no_content }
+        format.html { redirect_back fallback_location: root_path, notice: 'User unfollowed with success.'}
+      else
+        format.json { render json: nil, status: :unprocessable_entity }
+        format.html { redirect_back fallback_location: root_path }        
+      end
+    end
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
