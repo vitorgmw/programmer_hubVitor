e0036717fef93032e5be2aea788ad13891a5f5f2
vitorgmw <vitor_martines@hotmail.com>
2018-01-11 12:16:44 -0200

    criadas paginas de erro

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
   root 'pages#home'
 
   resources :comments, only: [:create, :destroy, :edit, :update]
